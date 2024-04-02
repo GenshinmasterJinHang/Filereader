5cc55a4e2c83a513214ba4f2f4140423c02de262
GenshinmasterJinHang <161272990+GenshinmasterJinHang@users.noreply.github.com>
2024-04-02 11:22:27 +0800

    add filewriter

--- grocery/src/App.java
+++ grocery/src/App.java
@@ -1,48 +1,39 @@
 import java.io.FileReader;
-import java.io.IOException;
+import java.io.FileWriter;
+import java.text.DecimalFormat;
 import java.io.BufferedReader;
-import java.io.File;
+import java.io.BufferedWriter;
+
 
 public class App {
-    public static void main(String[] args) {
-        File txt = new File("D:\\workspace\\Groceries.txt");
-        FileReader nh = null;  
-        BufferedReader bk = null; 
+    public static void main(String[] args) throws Exception{
+       String txt="D:\\workspace\\Groceries.txt";
+       String txt1="D:\\workspace\\1.txt";
+        FileReader sc = new FileReader(txt);
+        FileWriter wr = new FileWriter(txt1);
+        BufferedReader nh = new BufferedReader(sc);
+        BufferedWriter nm = new BufferedWriter(wr);
         double sum = 0;
-        String line;  
-        try {  
-            nh = new FileReader(txt);  
-            bk = new BufferedReader(nh);  
-        while ((line = bk.readLine()) != null) {  
-            System.out.println(line);
-            String[] parts = line.trim().split(",");
-            if (parts.length > 0) {  
-                
-                String lastPart = parts[parts.length - 1];  
-                try {  
-                    double value = Double.parseDouble(lastPart);  
-                    sum += value;  
-                } catch (NumberFormatException e) {  
-                    System.err.println("Error");  
-                }  
-            }  
-        }  
-        System.out.printf("Total price is : " +"%.2f%n",sum);  
-    } catch (IOException e) {  
-        e.printStackTrace();  
-    } finally {  
-        try {  
-            if (bk != null) {  
-                bk.close();  
-            }  
-            if (nh != null) {  
-                nh.close();  
-            }  
-        } catch (IOException e) {  
-            e.printStackTrace();  
-        }  
-    }  
-}  
-  }
+        String imp;  
+        String partition = ",";
+       String[] array;
+       while ((imp=nh.readLine()) !=null) {
+             System.out.println(imp);
+             array = imp.split(partition);
+             double a = Double.parseDouble(array[3]);
+             sum+=a;
+             nm.write(imp);
+             nm.newLine();
 
-
+        }
+        DecimalFormat decimalFormat = new DecimalFormat("#.##");  
+        String formattedValue = decimalFormat.format(sum);  
+        System.out.print("Total price is : " +formattedValue);
+        nm.write("Total price is : "
+        +formattedValue);
+        nh.close();
+        nm.flush();
+        nm.close();
+    }
+}
+            
